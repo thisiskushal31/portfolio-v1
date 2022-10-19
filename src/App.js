48f4a7b03e21cd267f2b64974dfa450678df7629
thisiskushal31 <guptakushal131@gmail.com>
2022-10-19 23:23:24 +0530

    Intial File Setup Updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './Styles/main.scss';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Home About Education Skills Project Contacts
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<About />} />
+            <Route path="/signup" element={< />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
