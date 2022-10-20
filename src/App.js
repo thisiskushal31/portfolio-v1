420f7557d86fb1003fabbcb1b17748a3d4b9a9a3
thisiskushal31 <guptakushal131@gmail.com>
2022-10-20 21:47:34 +0530

    Intial File Setup Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Education from './Pages/Education';
+import Skills from './Pages/Skills';
+import Project from './Pages/Project';
+import Contact from './Pages/Contacts';
+
 import './Styles/main.scss';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Home About Education Skills Project Contacts
-
 
 function App() {
   return (
@@ -13,8 +20,11 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<About />} />
-            <Route path="/signup" element={< />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/projects" element={<Project />} />
+            <Route path="/contacts" element={<Contact />} />
           </Routes>
         </div>
       </BrowserRouter>
