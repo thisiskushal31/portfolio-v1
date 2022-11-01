745f304a91f8559a949db5c190cbec3e48336277
thisiskushal31 <guptakushal131@gmail.com>
2022-11-01 22:40:59 +0530

    Final UI Changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/thisiskushal31/portfolio'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      Created By Kushal Gupta
     </a>
   </footer>
 )
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,71 +1,70 @@
 const header = {
-  // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://github.com/thisiskushal31',
+  title: 'KG.',
 }
 
 const about = {
-  // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Kushal Gupta',
+  role: 'Full Stack Developer and Cloud Enthusiast',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'I am an final year undergraduate student based in India who is passionate about developing Full-Stack Applications and is interested in DevOps and Cloud technologies. Besides that, I seek to explore a variety of cutting-edge cloud technologies in the software development industry.',
+  resume: '/resume',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/thisiskushalgupta/',
+    github: 'https://github.com/thisiskushal31',
   },
 }
 
 const projects = [
-  // projects can be added an removed
-  // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Agility',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+      'Agilty is a task management tool that allows you to plan, organise, and prioritise all of your tasks so that you can complete them in the shortest amount of time possible built using ReactJS on the frontend and MongoDB on the backend.',
+    stack: ['SASS', 'ReactJS', 'MongoDB'],
+    sourceCode: 'https://github.com/thisiskushal31/Agility',
+    livePreview: 'https://youtu.be/v2W6W7GKgro',
+  },
+  {
+    name: 'SocialSplit',
+    description:
+      'SocialSplit is a real-time chat application built on the backend with NodeJS, Express, and Socket.io and the frontend with React.',
+    stack: ['ChakraUI', 'ReactJS', 'Socket.io', 'MongoDB'],
+    sourceCode: 'https://github.com/thisiskushal31/SocialSplit',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 2',
+    name: 'LinkSaver Chrome Extension',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'The LinkSaver Chrome extension saves all of your tabs. LinkSaver uses Javascript save in LocalStorage to save all tabs in the currently active Chrome window to your bookmarks.',
+    stack: ['Javascript', 'HTML', 'CSS'],
+    sourceCode: 'https://github.com/thisiskushal31/link-saver-extension',
+    livePreview: 'https://github.com/thisiskushal31/link-saver-extension',
   },
 ]
 
 const skills = [
-  // skills can be added or removed
-  // if there are no skills, Skills section won't show up
+  'C/C++',
+  'Java',
   'HTML',
   'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
   'SASS',
-  'Material UI',
+  'JavaScript',
+  'React',
+  'NodeJS',
+  'ExpressJS',
+  'MongoDB',
+  'MySQL',
   'Git',
   'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Docker',
+  'Kubernates',
+  'Google Cloud',
+  'AWS',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'guptakushal070@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
